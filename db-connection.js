b90d8be41737907ec4ae60c175f3c4c1f8e80e70
EngJun <109129614+EngJun@users.noreply.github.com>
2022-07-12 15:07:51 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'ssh -i "AWSDemo.pem" ubuntu@ec2-54-174-250-183.compute-1.amazonaws.com',
     port: '3306',
-    user: 'root',
-    password: 'root',
+    user: 'admin',
+    password: 'password',
     database: 'movie_info'
 });
 
 connection.connect(err => {  // test out connetion and console.log error if there is one
     if (err) throw err;
-    console.log('Connected To DB');
+    console.log('Connected To AWSDB');
 }); 
 module.exports = connection;
 
